ec83d4e5c2f95a64d8db7d5dbd5fdefefc7e5ae1
Tsungyu Yu <ytyubox@gmail.com>
2020-05-21 18:52:57 +0800

    add all itemView, add panGesture

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -8,30 +8,53 @@
 
 import UIKit
 
-struct ViewInfo {
-    var rows:Int
-    var columns:Int
-    var bound: CGRect
-    func width()-> CGFloat {
-        bound.width / CGFloat(rows)
-    }
-    func height()-> CGFloat {
-        bound.height / CGFloat(columns)
-    }
-}
 
 class ViewController: UIViewController {
-
+    private
+    lazy var info =  ViewInfo(rows: 15, bound: view.frame)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let width = info.width()
+        let height = info.width()
+        let jMax = Int(view.frame.height) / Int(width)
+        for j in 0 ... jMax {
+            
+            for i in 0...info.rows {
+                let item = UIView()
+                item.backgroundColor = randomColor()
+                item.frame = CGRect(
+                    x: CGFloat(i) * width,
+                    y: CGFloat(j) * width,
+                    width: width,
+                    height: height)
+                item.layer.borderWidth = 1
+                item.layer.borderColor = UIColor.black.cgColor
+                view.addSubview(item)
+            }
+        }
+        let panGesture = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(didPanOnView))
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func didPanOnView(_ gesture: UIPanGestureRecognizer) {
+        let loction = gesture.location(in: view)
         
     }
-
-
 }
 
 // MARK: - private Color builder
 private func randomColor() ->UIColor {
     let (r,g,b) = (CGFloat(drand48()),CGFloat(drand48()),CGFloat(drand48()))
     return UIColor(red: r, green: g, blue: b, alpha: 1)
+}
+
+private struct ViewInfo {
+    var rows:Int
+    var bound: CGRect
+    func width()-> CGFloat {
+        bound.width / CGFloat(rows)
+    }
 }
