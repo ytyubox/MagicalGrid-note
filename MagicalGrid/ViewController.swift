e092f479fb25e45d3b4d92ee15cc903b1163cda8
Tsungyu Yu <ytyubox@gmail.com>
2020-05-21 19:08:15 +0800

    using hush map to quickly lookup focused view

--- MagicalGrid/ViewController.swift
+++ MagicalGrid/ViewController.swift
@@ -12,25 +12,17 @@ import UIKit
 class ViewController: UIViewController {
     private
     lazy var info =  ViewInfo(rows: 15, bound: view.frame)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        let width = info.width()
-        let height = info.width()
+        let width = info.width
+        let height = info.width
         let jMax = Int(view.frame.height) / Int(width)
         for j in 0 ... jMax {
             
             for i in 0...info.rows {
-                let item = UIView()
-                item.backgroundColor = randomColor()
-                item.frame = CGRect(
-                    x: CGFloat(i) * width,
-                    y: CGFloat(j) * width,
-                    width: width,
-                    height: height)
-                item.layer.borderWidth = 1
-                item.layer.borderColor = UIColor.black.cgColor
-                view.addSubview(item)
+               let itemView = makeItemView(i, width, j, height)
+                view.addSubview(itemView)
             }
         }
         let panGesture = UIPanGestureRecognizer(
@@ -41,6 +33,11 @@ class ViewController: UIViewController {
     
     @objc func didPanOnView(_ gesture: UIPanGestureRecognizer) {
         let loction = gesture.location(in: view)
+        let i = Int(loction.x / info.width)
+        let j = Int(loction.y / info.width)
+        let key = s(i,j)
+        let item = viewDic[key]!
+        view.bringSubviewToFront(item)
         
     }
 }
@@ -52,9 +49,34 @@ private func randomColor() ->UIColor {
 }
 
 private struct ViewInfo {
-    var rows:Int
-    var bound: CGRect
-    func width()-> CGFloat {
-        bound.width / CGFloat(rows)
+    internal init(rows: Int, bound: CGRect) {
+        self.rows = rows
+        self.width =  bound.width / CGFloat(rows)
     }
+    
+    var rows:Int
+    var width: CGFloat
 }
+
+private func s(_ i: Int...) -> String {
+    i.map(String.init).joined(separator: ":")
+}
+private
+var viewDic:[String: UIView] = [:]
+
+private
+  func makeItemView(_ i: Int, _ width: CGFloat,
+                    _ j: Int, _ height: CGFloat)-> UIView {
+      let item = UIView()
+      let a = s(i,j)
+      viewDic[a] = item
+      item.backgroundColor = randomColor()
+      item.frame = CGRect(
+          x: CGFloat(i) * width,
+          y: CGFloat(j) * width,
+          width: width,
+          height: height)
+      item.layer.borderWidth = 1
+      item.layer.borderColor = UIColor.black.cgColor
+      return item
+  }
